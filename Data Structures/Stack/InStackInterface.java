a77763ac5c32a032a23aa4051df3df9ba4641627
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2020-08-25 20:55:04 -0400

    Create InStackInterface.java

---
+++ Data Structures/Stack/InStackInterface.java
@@ -0,0 +1,15 @@
+/*
+ * InStackInterface.java
+ * Author: Nicholas Kawwas
+ * Completed: August, 2018
+ * Purpose: Stack Interface (Abstract)
+*/
+
+package GenStackInterface;
+
+public interface InStackInterface{
+	public boolean isEmpty();
+ 	public void push();
+ 	public int pop();
+ 	public int peek();
+}
